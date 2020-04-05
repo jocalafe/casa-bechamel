605cd7e472018166c20dc82f68377c12ce2ab1ab
jocalafe <jocalafe@gmail.com>
2020-04-05 21:09:01 +0200

    refactor: provide Card.Header and Card.body as compound components

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledCard = styled.div`
+  box-shadow: 0px 3px 5px rgba(0,0,0,0.5);
+  border-radius: 5px;
+  background-color: white;
   display: flex;
   flex-direction: column;
   padding: 30px;
-  box-shadow: 0px 3px 5px rgba(0,0,0,0.5);
-  border-radius: 5px;
+  text-align: center;
+
+  :hover {
+    box-shadow: 0px 5px 10px rgba(0,0,0,0.5);
+  }
 `;
 
 export interface CardProps {
@@ -17,3 +23,23 @@ export interface CardProps {
 export default function Card({ className, children }: CardProps) {
   return <StyledCard className={ className }>{ children }</StyledCard>
 };
+
+const StyledHeading = styled.h2`
+  font-weight: normal;
+`;
+
+function Heading({ text }: { text: string }) {
+  return <StyledHeading>{ text }</StyledHeading>;
+}
+
+const StyledBody = styled.div`
+  padding: 20px;
+`;
+
+function Body({ children }: { children: React.ReactNode }) {
+  return <StyledBody>{ children }</StyledBody>;
+}
+
+Card.Heading = Heading;
+Card.Body = Body;
+
--- src/WelcomeCard.tsx
+++ src/WelcomeCard.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import Card from './Card';
-
-const StyledCard = styled(Card)`
-  background-color: white;
-  text-align: center;
-`;
 
 export default function WelcomeCard() {
   return (
-    <StyledCard>
-      <h2>Comida hecha con amor, en tu casa u oficina.</h2>
-      <div>Has tu pedido ya!</div>
-    </StyledCard>
+    <Card>
+      <Card.Heading text="Comida hecha con amor, en tu casa u oficina." />
+      <Card.Body>Has tu pedido ya!</Card.Body>
+    </Card>
   )
 }
